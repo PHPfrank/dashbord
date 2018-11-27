fec72773d960dbdbff1ef31e36a28ef2d96afbb1
PHPfrank <t3056339@163.com>
2018-11-27 18:04:49 +0800

    首页功能

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,7 +1,8 @@
 import { Component } from 'react';
 //注意这里我们除了从antd中引入了Layout布局组件，还引入了Menu菜单组件，Icon图标组件
 import Link from 'umi/link';
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu, Icon ,Modal, message,Dropdown,Button,Input} from 'antd';
+import axios from 'axios';
 
 // 引入子菜单组件
 const SubMenu = Menu.SubMenu;
@@ -9,8 +10,72 @@ const SubMenu = Menu.SubMenu;
 // Header, Footer, Sider, Content组件在Layout组件模块下
 const { Header, Footer, Sider, Content } = Layout;
 
+
 export default class BasicLayout extends Component {
+
+    state = { 
+        visible: false ,
+      }
+
+    showModal = () => {
+      this.setState({
+        visible: true,
+      });
+    }
+
+    hideModal = () => {
+      this.setState({
+        visible: false,
+      });
+    }
+
+    //修改密码
+    editPwd = () => {
+      var origin_pwd = document.getElementById("origin_pwd").value;
+      var new_pwd = document.getElementById("new_pwd").value;
+      var url = "/admin/react/editUserPwd";
+      const _this = this;
+      axios.post(url, {
+          origin_pwd: origin_pwd,
+          new_pwd: new_pwd,
+        })
+        .then(function (response) {
+            if(response.data.error == 0){
+               //console.log(response.data);
+                //全局提示
+               message.success('修改成功',1);
+               _this.setState({
+                visible: false,
+              }); 
+            }else{
+                //全局提示
+               message.error('修改失败',1);
+            }
+        })
+    }
+
+    //用户退出
+    userLogout = () => {
+      var url = "/admin/react/userLogout";
+      const _this=this;
+      axios.post(url)
+        .then(function (response) {
+            if(response.data.error == 0){
+                //全局提示
+               message.success('退出成功',1);
+               //延时跳转
+               _this.timer = setTimeout(()=>{
+                _this.props.history.push('/');
+                },1500);            
+            }else{
+                //全局提示
+               message.error('退出失败',1);
+            }
+        })
+    }
+
     render() {
+   
         return (
           <Layout>
             <Sider width={256} style={{ minHeight: '100vh' }}>
@@ -33,19 +98,56 @@ export default class BasicLayout extends Component {
               </Menu>
             </Sider>
             <Layout >
-              <Header style={{ background: '#fff', margin: '0 0 0 0' }}>
-                <Icon
+              <Header style={{ background: '#fff', margin: '0 0 0 0' }}>                
+                  <Icon
                   className="trigger"
                   type={'menu-fold'}
                   style={{ fontSize: '30px'}}
-                />
+                  />              
+                <div style={{float:'right'}}>
+                <Icon className="trigger" type="github" style={{ fontSize: '30px',marginRight:'30px'}}/>
+                {/* <Dropdown overlay={menu} trigger={['click']}>
+                  <a className="ant-dropdown-link" href="#">
+                    <Icon type="setting" style={{ fontSize: '30px',}}/>
+                    设置
+                  </a>                   */}
+                  {/* <Button icon="setting" size = "large">设置</Button> */}
+                {/* </Dropdown> */}
+                <Dropdown overlay={
+                  <Menu>
+                  <Menu.Item key="0" >
+                    <a onClick={this.showModal}>修改密码</a>
+                  </Menu.Item>
+                  <Menu.Item key="1">
+                    <a onClick={this.userLogout}>退出登录</a>
+                  </Menu.Item>
+                </Menu>
+                } trigger={['click']}>
+                <a className="ant-dropdown-link" href="#">
+                    <Icon type="setting" style={{ fontSize: '30px',}}/>
+                    设置
+                </a>                
+                </Dropdown>
+                <Modal
+                      title="Modal"
+                      visible={this.state.visible}
+                      onOk={this.editPwd}
+                      onCancel={this.hideModal}
+                      destroyOnClose
+                      okText="确认"
+                      cancelText="取消"
+                >
+                <Input id="origin_pwd" autoComplete="off" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="原密码" />
+                <Input id="new_pwd" autoComplete="off" style={{ marginTop:'30px'}} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="新密码" />
+                </Modal>
+                </div>                
               </Header>
               <Content style={{ margin: '24px 16px 0' }}>
                 <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
                   {this.props.children}
                 </div>
               </Content>
-              <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+              <Footer style={{ textAlign: 'center' }}>Powered by Frank</Footer>
             </Layout>
           </Layout>
         )
